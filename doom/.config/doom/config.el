8a174c679e093d5611eb89157e7b98d75518b83b
Daniel <danielhunterblue@gmail.com>
2025-01-07 16:47:29 -0800

    Update config.el, auto launch resizing

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -9,6 +9,26 @@
 (setq user-full-name "Daniel Blue"
       user-mail-address "danielhunterblue@gmail.com")
 
+;; sets the emacs window to be a good size :) 
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 90))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 220)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
