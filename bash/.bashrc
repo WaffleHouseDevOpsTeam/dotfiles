08cc430e45f6b603bc623c3b1cab868e3b648f4c
Daniel <danielhunterblue@gmail.com>
2025-01-08 15:01:35 -0800

    updated .bashrc, updated init.lua for nvim

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,14 +1,16 @@
 #
 # ~/.bashrc
-#
+# Author Daniel Blue
 
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-alias ls='ls -l --color=auto'
+alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-PS1='[\u@\h \W]\$ '
+PS1='\[\033[1;34m\][\t] \[\033[32m\]\u@\h \[\033[37m\]> \[\033[31m\]\w \[\033[1;37m\]\n$\[\033[0;37m\] '
+
 export PATH=/home/daniel/.cargo/bin:$PATH
 export PATH=/home/daniel/.config/emacs/bin:$PATH
-fortune | cowsay -r | lolcat
+
+fortune | cowsay -f tux | lolcat
 
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,9 +10,9 @@ vim.cmd("set path+=**")
 vim.cmd("set wildmenu")
 
 vim.cmd("let g:netrw_banner=0")        -- disable annoying banner
-vim.cmd("let g:netrw_browse_split=4")  -- open in prior window
-vim.cmd("let g:netrw_altv=1")          -- open splits to the right
-vim.cmd("let g:netrw_liststyle=3")     --tree view
+-- vim.cmd("let g:netrw_browse_split=4")  -- open in prior window
+-- vim.cmd("let g:netrw_altv=1")          -- open splits to the right
+-- vim.cmd("let g:netrw_liststyle=3")     --tree view
 vim.cmd("let g:netrw_list_hide=netrw_gitignore#Hide()")
 -- vim.cmd("let g:netrw_list_hide.=',\(^\|\s\s\)\zs\.\S\+'")
 
