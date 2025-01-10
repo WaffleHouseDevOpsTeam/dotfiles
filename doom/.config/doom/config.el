0f67f9eb539df5f6bc1b027b6dbd2814ca987bd7
Daniel <danielhunterblue@gmail.com>
2025-01-09 21:05:40 -0800

    updated some things

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -61,7 +61,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Documents/college")
+(setq org-directory '("~/Documents/college/Spring2025/"))
+(setq org-agenda-files '("~/Documents/college/Spring2025/"))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
